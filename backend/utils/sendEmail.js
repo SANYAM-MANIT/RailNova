62c9c43aa51c2a9d29a278cbac240393e7b240be
Sanyam Jain <sanyamjain@Sanyams-MacBook-Air.local>
2026-07-15 01:07:54 +0530

    Use Gmail SMTP 587

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,32 +3,23 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
 
     auth: {
-
         user: process.env.EMAIL_USER,
-
         pass: process.env.EMAIL_PASS
-
     }
-
 });
 
 const sendEmail = async (to, subject, html) => {
-
     await transporter.sendMail({
-
         from: `"RailNova" <${process.env.EMAIL_USER}>`,
-
         to,
-
         subject,
-
         html
-
     });
-
 };
+
 module.exports = sendEmail;
