a0f0224e742fea986816a59db9a630c38916518f
Sanyam Jain <sanyamjain@Sanyams-MacBook-Air.local>
2026-07-15 12:36:03 +0530

    Switch to Brevo SMTP

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,23 +3,37 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+
+    host: "smtp-relay.brevo.com",
+
     port: 587,
+
     secure: false,
 
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+
+        user: process.env.BREVO_USER,
+
+        pass: process.env.BREVO_PASS
+
     }
+
 });
 
 const sendEmail = async (to, subject, html) => {
+
     await transporter.sendMail({
+
         from: `"RailNova" <${process.env.EMAIL_USER}>`,
+
         to,
+
         subject,
+
         html
+
     });
+
 };
 
 module.exports = sendEmail;
