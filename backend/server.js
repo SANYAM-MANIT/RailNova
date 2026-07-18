7f99ade9629580340f91531676ae233ee4532658
Sanyam Jain <sanyamjain@Sanyams-MacBook-Air.local>
2026-07-18 11:09:28 +0530

    Update CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,17 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
-// const cors = require("cors");
+//app.use(cors());
+const cors = require("cors");
 
-// app.use(cors({
-//     origin: [
-//         "http://localhost:5500",
-//         "http://127.0.0.1:5500",
-//         "https://rail-nova-sanyam.vercel.app"
-//     ],
-//     credentials: true
-// }));
+app.use(cors({
+    origin: [
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://rail-nova-sanyam.vercel.app"
+    ],
+    //credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/trains",trainRoutes);
